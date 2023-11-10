466281985ccf2f6c46e578acb7898cffc6ed2a6c
Kavindu Piyumantha <tkp99x@gmail.com>
2023-11-10 09:49:45 +0530

    created a simple chat screen
    built a simple chat screen efficiently showing a list of expandable & animated messages containing an image and texts, designed using Material Design principles with a dark theme included and previews

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.example.composeapplication
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +39,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MassageCard(Massage("Android", "Jetpack Compose"))
+            ComposeApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    MessageCard(Massage("Android", "Jetpack Compose"))
+
+                    Conversation(
+                        listOf(
+                            Massage("Kavindu", "Hey,take a look at Jetpack Compose,its great!"),
+                            Massage("Android", "Jetpack Compose is the future of Android development!"),
+                            Massage("Kavindu", "I think you're right!"),
+                            Massage("Android", "It's completely different from anything we've seen before!"),
+                            Massage("Kavindu", "Yes, and we're going to master it!"),
+                            Massage("Android", "You bet we are!")
+                        )
+                    )}
+            }
+
         }
     }
 }
@@ -33,30 +65,88 @@ class MainActivity : ComponentActivity() {
 data class Massage(val author: String, val body: String)
 
 @Composable
-fun MassageCard(msg: Massage) {
-    Row {
+fun MessageCard(msg: Massage) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.ic_launcher_background),
             contentDescription = "Contact profile picture",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
-
         Spacer(modifier = Modifier.size(8.dp))
 
-        Column {
-            Text(text = "From ${msg.author}")
+        var isExpanded by remember { mutableStateOf(false) }
+
+        val surfaceColor by animateColorAsState(
+           if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column (modifier = Modifier.clickable {isExpanded = !isExpanded}) {
+            Text(
+                text = "From ${msg.author}",
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
             Spacer(modifier = Modifier.size(4.dp))
-            Text(text = msg.body)
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ){
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Massage>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
         }
     }
 }
 
 @Preview
 @Composable
+fun PreviewConversation() {
+    ComposeApplicationTheme {
+        Conversation(
+            listOf(
+                Massage("Kavindu", "Hey,take a look at Jetpack Compose,its great!"),
+                Massage("Android", "Jetpack Compose is the future of Android development!"),
+                Massage("Kavindu", "I think you're right!"),
+                Massage("Android", "It's completely different from anything we've seen before!"),
+                Massage("Kavindu", "Yes, and we're going to master it!"),
+                Massage("Android", "You bet we are!")
+            )
+        )
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
 fun PreviewMassageCard() {
-    MassageCard(
-        Massage("Kavindu", "Hey,take a look at Jetpack Compose,its great!")
-    )
+    ComposeApplicationTheme {
+        Surface {
+            MessageCard(
+                Massage("Kavindu", "Hey,take a look at Jetpack Compose,its great!")
+            )
+        }
+    }
 }
